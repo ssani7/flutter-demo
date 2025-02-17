9a342ae4507df6cdacc58d6933a5d04a0888e9fe
ssani7 <sanaullah.sani756@gmail.com>
2025-02-17 10:07:13 +0600

    updated resoultion

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sanaullah Project',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFEFEFEF)),
       home: const MyHomePage(title: 'Sanaullah Sani'),
     );
   }
@@ -61,36 +59,27 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset('assets/images/sani-photo.png'),
-            const Text('I am Sanaullah Sani'),
+            Padding(
+              padding: EdgeInsets.fromLTRB(40, 0, 40, 40),
+              child: Column(
+                children: [Image.asset('assets/images/sani-photo.png')],
+              ), //Container
+            ),
+            const Text(
+              'I am Sanaullah Sani',
+              style: TextStyle(color: Colors.white),
+            ),
           ],
         ),
       ),
